1c2fcf1f9660492267c96660fb339106eb27fb55
can-eat-a-fish <1214383460@qq.com>
2017-12-09 22:27:21 +0800

    first commit

---
+++ src/com/example/wzq/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.wzq;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	
+
+}
